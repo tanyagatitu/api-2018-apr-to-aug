28d13d624552f487807719643c22a0f2f3a8d742
tanyagatitu <tanyagatitu@gmail.com>
2018-05-10 14:05:35 +0300

    first commit

--- rewind1/bbit3c.php
+++ rewind1/bbit3c.php
@@ -1,22 +1,79 @@
+
 <?php
-/*
-  Consider the following string(bbit3c.txt)
-  It constains names(surname,second_name,first_name),number,country and country code of some individuals
+$bbit3c="Chirakahula,Mukwana Joe.0500387482.UGANDA.+256|
+ Wambua,Doe Mumbua.080023450.KENYA.+254|
+ Vonbora,Katherina Luther.0600990033.GERMANY.+32
+ |Boromir,Grommit Wallace.0100873901.IRELAND.+98
+ |Piepont,Edwards Jonathan.0200983729.AMERICA.+1
+ |Asaph,Elihu Mose.02990033.ISRAEL.+2
+ |Otoyo,Wafula Joel.0777799920.KENYA.+254
+ |Mapete,Mwambingu Simba.0987483292.TANZANIA.+257
+ |SARATON,DAVID BREINARD.043899292.AMERICA.+1
+ |POMPI,JOHN OWEN.074749292.ZAMBIA.+267
+ |MAG,JOHN BUNYAN.09837328.UGANDA.+256
+ |Vonstaupitz,Johann Gregory.0600984733.GERMANY.+32
+";
 
-  @Required
-  1. Create a new branch from the develop branch called feature-rewind-1
-  2. Display the following data as follows
+$bt3c = str_replace("|",".",$bbit3c);
+$bit3c= explode(".",$bt3c);
+ $b=array_chunk($bit3c,4);
 
-      a). first_name second_name,surname (# country_code(0) number[country])
-      b). .......
-      c). .......
+ echo "<ol>";
+ $phone;
+ $country;
+ foreach ($b as $a) {
+   // code...
+   echo "<li>";
+   foreach ($a as $key=>$value) {
+     // code...
 
-    Summary
-     -- Total names : total.
-     -- Total unique countries : total(list of countries - comma separated)
-  3. Commit changes of the new branch
-  4. Merge changes with the development branch
-  5. Push the changes to your repo.
-  6. Submit the github url on google classroom (Check Quiz 1)
- */
+     if ($key==0) {
+       // code...
+       $st=explode(",",$value);
+       $stq=explode(" ",$st[1]);
+       $st1=substr_replace($value,", ".substr($value,0,strpos($value,',')),-1);
+
+       $st1=$stq[1]." ".$stq[0].", ".$st[0];
+       $value=$st1;
+echo "{$value} ";
+     }
+     if ($key==3) {
+       // code...
+
+       $value="(#".$value." " . $phone. " [".$country."])";
+echo "{$value} ";
+     }
+     if ($key==1) {
+
+       // code...
+       global $phone;
+       $phone = $value;
+       $phone=substr_replace($value,"(0)".substr($value,1),0);
+
+// echo "{$value}";
+     }
+     if ($key==2) {
+       // code...
+       global $country;
+       $country = $value;
+
+
+     }
+
+   }
+   echo "</li>";
+ }
+ echo "</ol>";
+ echo "Summary: <p> total names: ";
+ echo "total unique countries ";
  ?>
+
+
+
+
+
+
+
+ ?>
+</body>
+</html>
